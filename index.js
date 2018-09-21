33c63175a7a6c4dc80114eb0663d602658a993b1
Zachary Hansen <zacharyhansen@Zacharys-MacBook-Pro.local>
2018-09-20 20:06:30 -0700

    add amazon links for bundles and refactor to use proxy server

--- index.js
+++ index.js
@@ -5,11 +5,63 @@ const path = require('path');
 const app = express();
 const port = 3000;
 const cors = require('cors')
+const axios = require('axios');
 
 app.use(cors());
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(morgan('dev'));
+
+app.use('/songs/:id', express.static('public'));
+
+app.get('/:id', (req, res) => {
+  res.send('I HAVE REACHED THE PROXY  params->  ' + req.params.id);
+})
+
+app.get('/relatedTracks/:id', (req, res) => {
+  // res.send(req.params.id)
+  const songId = req.params.id;
+  axios.get(`http://localhost:3002/relatedTracks/${songId}`)
+    .then(({ data }) => {
+      console.log('Related Tracks', data);
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get('/relatedAlbums/:id', (req, res) => {
+  const songId = req.params.id;
+  axios.get(`http://localhost:3002/relatedAlbums/${songId}`)
+    .then(({ data }) => {
+      console.log('Related Albums', data);
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get('/api/stats/:id', (req, res) => {
+  axios.get(`http://localhost:3004/api/stats/${req.params.id}`)
+    .then(({ data }) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+})
+
+app.get('/comments/:songid', (req, res) => {
+  // console.log(`http:/localhost:3001/api/${req.params.songid}`);
+  axios.get(`http://localhost:3001/api/${req.params.songid}`)
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
